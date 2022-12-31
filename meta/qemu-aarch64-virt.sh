8530a0aa6a9cc143db30826a9c8fca7dad7b0f01
Ryan Cohen <rcohenprogramming@gmail.com>
2022-12-31 14:05:32 -0500

    aarch64/entry: Got an entry point that compiles and loads properly
    However, it does not properly output to the UART. Still trying to figure
    out why.

---
+++ meta/qemu-aarch64-virt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This does load the elf image properly (confirmed with the qemu monitor), the program does not correctly output to the
+# virtual PL011 UART device. I want to debug the program with GDB to figure out why. Also, for some reason, the PC
+# of the VM ends up at 0x200, so there might be an instruction thats branching to the wrong address.
+
+# Run elf image with generic loader https://qemu.readthedocs.io/en/latest/system/generic-loader.html#setting-a-cpu-s-program-counter
+# Starts at address 0x40100000 to avoid collisions with the dtb. Not sure why, but the dtb is loaded at 0x40000000-0x40100000
+qemu-system-aarch64 -machine virt -cpu cortex-a57 -device loader,file=target/aarch64-unknown-none/debug/caliga-qemu-aarch64,addr=0x40100000,cpu-num=0 -S
+
--- src/arch/aarch64/entry.rs
+++ src/arch/aarch64/entry.rs
@@ -1,17 +1,26 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("start.S"));
 
 #[panic_handler]
-fn handle_panic(info: &PanicInfo) -> ! {
+fn handle_panic(info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn _start() {
-
-    let uart = 0x09000000 as *mut u8;
-    unsafe {core::ptr::write_volatile(uart, 'a' as u8)};
+#[link_section = ".text.boot"]
+pub extern "C" fn qemu_entry() {
+    // Taken from https://lowenware.com/blog/aarch64-bare-metal-program-in-rust/
+    const uart: *mut u8 = 0x09000000 as *mut u8;
+    let out_str = b"Hello aarch64";
+    for byte in out_str {
+        unsafe {
+            core::ptr::write_volatile(uart, *byte);
+        }
+    }
 
     loop {}
 }
