63386ab2c46fdeaf318c3792388b01c435dba4f0
Ryan Cohen <rcohenprogramming@gmail.com>
2023-01-01 08:49:53 -0500

    aarch64/entry: Got serial output working in assembler
    For some reason, the Rust code doesn't properly output to the UART,
    while the assembly code does. I wonder if it could be something to do
    with improper linking or relocation? It seems that one of the branch
    instructions might be going to the wrong address and not running the
    Rust UART code.

--- meta/qemu-aarch64-virt.sh
+++ meta/qemu-aarch64-virt.sh
@@ -6,5 +6,5 @@
 
 # Run elf image with generic loader https://qemu.readthedocs.io/en/latest/system/generic-loader.html#setting-a-cpu-s-program-counter
 # Starts at address 0x40100000 to avoid collisions with the dtb. Not sure why, but the dtb is loaded at 0x40000000-0x40100000
-qemu-system-aarch64 -machine virt -cpu cortex-a57 -device loader,file=target/aarch64-unknown-none/debug/caliga-qemu-aarch64,addr=0x40100000,cpu-num=0 -S
+qemu-system-aarch64 -machine virt -cpu cortex-a57 -device loader,file=target/aarch64-unknown-none/debug/caliga-qemu-aarch64,addr=0x40100000,cpu-num=0 -S -serial stdio
 
