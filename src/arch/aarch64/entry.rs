f5bfdd1ef65868ad64300bbbabe259e3454796ca
Ryan Cohen <rcohenprogramming@gmail.com>
2023-01-02 14:58:53 -0500

    aarch64/entry: Got `write_volatile` to not crash!
    Finally figured out (with GDB) that the reason for the crash was the use
    of floating point registers for the `cnt` instruction. This was used by
    the rust function `u8::count_ones`.
    
    Here is a partial stack trace with some intermediate functions omitted:
    
    ```
    qemu_entry
    ptr::write_volatile
    u8::is_aligned_to
    u8::is_power_of_two
    u8::count_ones
    instrinsics::ctpop (alias of `cnt` instruction)
    
    CRASH
    ```
    
    By disabling the floating point LLVM feature in the target's json spec,
    `intrinsics::ctpop` is changed to use general instructions instead.
    This allows for usage of `write_volatile` without crashing the system!

--- src/arch/aarch64/entry.rs
+++ src/arch/aarch64/entry.rs
@@ -15,14 +15,11 @@ fn handle_panic(info: &core::panic::PanicInfo) -> ! {
 #[link_section = ".text.boot"]
 pub extern "C" fn qemu_entry() {
     // Taken from https://lowenware.com/blog/aarch64-bare-metal-program-in-rust/
-    const uart: *mut u8 = 0x09000000 as *mut u8;
-    let out_str = b"Hello aarch64";
+    let uart = 0x0900_0000 as *mut u8;
+    let out_str = b"Hello aarch64\n";
     for byte in out_str {
         unsafe {
-            core::intrinsics::volatile_store(uart, *byte);
-            // For some reason, `write_volatile` panics.
-            // Maybe it thinks that the UART pointer is null?
-            //core::ptr::write_volatile(uart, *byte);
+            core::ptr::write_volatile(uart, *byte);
         }
     }
 
