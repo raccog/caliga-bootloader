14375ae4045be7b798bc5cb0b5006423296be332
Ryan Cohen <rcohenprogramming@gmail.com>
2023-01-06 00:37:07 -0500

    aarch64: Add `Write` trait to `UartPl011`

--- bin/aarch64/qemu.rs
+++ bin/aarch64/qemu.rs
@@ -1,35 +1,48 @@
 #![no_std]
 #![no_main]
 
-use core::{ptr, arch::global_asm};
+use core::{ptr, fmt::{self, Write}, arch::global_asm };
 
 global_asm!(include_str!("start.S"));
+
+pub const UART0_ADDR: usize = 0x0900_0000;
+
+pub struct UartPl011 {
+    base: *mut u8,
+}
+
+impl UartPl011 {
+    pub fn new(base: *mut u8) -> Result<Self, ()> {
+        match base as usize {
+            UART0_ADDR => Ok(Self{base}),
+            _ => Err(())
+        }
+    }
+
+    pub fn write_byte(&mut self, byte: u8) {
+        unsafe { ptr::write_volatile(self.base, byte); }
+    }
+}
+
+impl fmt::Write for UartPl011 {
+    fn write_str(&mut self, out_string: &str) -> fmt::Result {
+        for out_byte in out_string.bytes() {
+            self.write_byte(out_byte);
+        }
+        Ok(())
+    }
+}
 
 #[panic_handler]
 fn handle_panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-struct UartPl011 {
-    pub base: *mut u8,
-}
-
-impl UartPl011 {
-    pub fn write_byte(&mut self, byte: u8) {
-        unsafe { ptr::write_volatile(self.base, byte); }
-    }
-}
-
 #[no_mangle]
 #[link_section = ".text.boot"]
 pub extern "C" fn qemu_entry() {
-    // Taken from https://lowenware.com/blog/aarch64-bare-metal-program-in-rust/
-    const UART_ADDR: usize = 0x0900_0000;
-    let mut uart = UartPl011 { base: UART_ADDR as *mut u8 };
-    let out_str = b"Hello aarch64\n";
-    for byte in out_str {
-        uart.write_byte(*byte);
-    }
+    let mut uart = UartPl011::new(UART0_ADDR as *mut u8).unwrap();
+    uart.write_str("Hello aarch64\n").unwrap();
 
     loop {}
 }
