3bf4637c0c7f4a62793d0c8f05ee117cc94e56a7
Ryan Cohen <rcohenprogramming@gmail.com>
2023-03-14 12:25:41 -0400

    cpuid: Shrink CPUID max value range in unit test

--- src/developing_modules/x86_64/cpuid.rs
+++ src/developing_modules/x86_64/cpuid.rs
@@ -9,9 +9,10 @@ use std::println as debug;
 #[derive(Debug)]
 pub struct CpuidMaxValues {
     pub basic: u32,
-    pub extended: u32
+    pub extended: u32,
 }
 
+/// Returns the maximum values for CPUID basic and extended functions.
 pub unsafe fn cpuid_max_values() -> CpuidMaxValues {
     let (basic, _) = __get_cpuid_max(0);
     let (extended, _) = __get_cpuid_max(0x8000_0000);
@@ -24,19 +25,25 @@ mod tests {
     use super::*;
 
     /// A simple check to ensure that `cpuid_max_values`'s result is in a valid range.
-    /// 
+    ///
     /// Since the max values are different on each CPU, this just ensures that the basic
-    /// max value is not 0 and is less than 0x8000_0000 (start of extended values). The
-    /// extended max value is checked to be greater than 0x8000_0000, but less than 0x9000_0000.
-    /// The value 0x9000_0000 was chosen because it seems that the latest Intel and AMD processors
-    /// don't use anything past that value.
+    /// max value is greater than 0 and is less than 0x1000_0000. The
+    /// extended max value is checked to be greater than 0x8000_0000 and less than 0x9000_0000.
+    ///
+    /// These ranges were chosen because the Intel and AMD manuals do not list any CPUID leaves
+    /// that are out of range.
     #[test]
     fn cpuid_max() {
-        let CpuidMaxValues {basic, extended} = unsafe { cpuid_max_values() };
-        debug!("CPUID Max Values {{ basic: {:#x}, extended: {:#x} }}", basic, extended);
+        let CpuidMaxValues { basic, extended } = unsafe { cpuid_max_values() };
+        debug!(
+            "CPUID Max Values {{ basic: {:#x}, extended: {:#x} }}",
+            basic, extended
+        );
+        const BASIC_MIN: u32 = 0;
+        const BASIC_MAX: u32 = 0x1000_0000;
         const EXTENDED_MIN: u32 = 0x8000_0000;
         const EXTENDED_MAX: u32 = 0x9000_0000;
-        assert!(basic > 0 && basic < EXTENDED_MIN);
+        assert!(basic > BASIC_MIN && basic < BASIC_MAX);
         assert!(extended > EXTENDED_MIN && extended < EXTENDED_MAX);
     }
 }
